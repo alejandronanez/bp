eda0ea210ec3ca159a68dedbd75b427ef03ac1fd
Alejandro Nanez <alejonanez@gmail.com>
2016-06-24 00:04:39 -0500

    Updated build process

--- tooling/parts.js
+++ tooling/parts.js
@@ -31,4 +31,16 @@ exports.setupCSS = function(paths) {
 			]
 		}
 	}
+};
+
+exports.minify = function() {
+	return {
+		plugins: [
+			new webpack.optimize.UglifyJsPlugin({
+				compress: {
+					warnings: false
+				}
+			})
+		]
+	}
 }
--- webpack.config.js
+++ webpack.config.js
@@ -31,12 +31,19 @@ switch(process.env.npm_lifecycle_event) {
 	case 'build':
 		config = merge(
 			common,
+			{
+				devtool: 'source-map'
+			},
+			parts.minify(),
 			parts.setupCSS(PATHS.app)
 		);
 		break;
 	default:
 		config = merge(
 			common,
+			{
+				devtool: 'eval-source-map'
+			},
 			parts.setupCSS(PATHS.app),
 			parts.devServer({
 				host: process.env.HOST,
