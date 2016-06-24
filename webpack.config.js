6246531272f4a4d5003203d647bc40f0ec6f1c10
Alejandro Nanez <alejonanez@gmail.com>
2016-06-23 23:35:03 -0500

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
 
 const PATHS = {
 	app: path.join(__dirname, 'app'),
 	build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
 	entry: {
 		app: PATHS.app
 	},
@@ -20,3 +22,15 @@ module.exports = {
 		})
 	]
 };
+
+var config;
+
+switch(process.env.npm_lifecycle_event) {
+	case 'build':
+		config = merge(common, {});
+		break;
+	default:
+		config = merge(common, {});
+}
+
+module.exports = validate(config);
