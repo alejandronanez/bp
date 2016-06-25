e04a546c6ff294a8eac63f1024e5174b4376e74f
Alejandro Nanez <alejonanez@gmail.com>
2016-06-24 22:47:10 -0500

    coverage

---
+++ app/bar/bar.js
@@ -0,0 +1,7 @@
+export function bar() {
+	return 1 + 1;
+}
+
+export function baz() {
+	return 1 + 1;
+}
--- app/foo/__tests__/foo.test.js
+++ app/foo/__tests__/foo.test.js
@@ -1,6 +1,3 @@
 import test from 'ava';
-import { foo } from '../foo';
 
-test('foo test', t => {
-	t.pass();
-});
+test.todo('foo module test');
